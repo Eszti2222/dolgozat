d602825e0267d0baedc89561e6a11a728c3580d2
Eszti2222 <nagy.eszter2222@gmail.com>
2024-11-07 09:59:44 +0100

    vege javitva

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import feladatok
-'''print("1.feladat")
+print("1.feladat")
 feladatok.paros_szamok()
 print("2.feladat")
 feladatok.veletlen_szam()
 print("3.feladat")
 feladatok.karakter("Ez az én dumám",6)
 print("4.feladat")
-feladatok.nevet_ker()'''
+feladatok.nevet_ker()
 print("5.feladat")
 feladatok.ko_papir_ollo()
